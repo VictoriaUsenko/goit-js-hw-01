151f1308d187db83948060be7f327a7a29477449
Victoria Usenko <usenko.vika@gmail.com>
2024-02-20 18:45:16 +0200

    +

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ function getElementWidth(content, padding, border){
     const b = Number.parseFloat(padding)*1000;
     const c = Number.parseFloat(border)*1000;
 
-    return (a +2*b+2*c)/1000; 
+    return (a + 2*b + 2*c)/1000; 
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
